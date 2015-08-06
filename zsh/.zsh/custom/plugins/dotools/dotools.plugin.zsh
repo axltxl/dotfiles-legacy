cb23e633c6a8e22ef46af309714056e9199d2a27
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-08-06 15:14:05 -0430

    Fancier version of dotu

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -1,4 +1,4 @@
 alias dotstow='stow   --verbose=1 -R -d $DOT_HOME -t $HOME'
 alias dotunstow='stow --verbose=1 -D -d $DOT_HOME -t $HOME'
 alias dotcd='cd $DOT_HOME'
-alias dotu='cd $DOT_HOME && git pull' # Update dotfiles
+alias dotu='git -C $DOT_HOME pull' # Update dotfiles
