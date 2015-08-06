c982bef261049b966c18d63c9bea4cd67e9406d2
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-08-06 15:11:27 -0430

    dotu: update dot files

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -1,3 +1,4 @@
 alias dotstow='stow   --verbose=1 -R -d $DOT_HOME -t $HOME'
 alias dotunstow='stow --verbose=1 -D -d $DOT_HOME -t $HOME'
 alias dotcd='cd $DOT_HOME'
+alias dotu='cd $DOT_HOME && git pull' # Update dotfiles
