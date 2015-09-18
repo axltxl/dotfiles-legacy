8d689a16774e5120eb4b7b813578a0497332843c
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-09-18 14:07:34 -0430

    a little bit more of consistency on install script

--- install.sh
+++ install.sh
@@ -27,21 +27,24 @@ rm -rf ~/.profile $DOT_HOME $DOT_PATH $ZSH $ZSH_HOME
 # DOT_PATH is created before used
 mkdir -p $DOT_PATH/{lib,bin,libexec,include,share}
 
+# Basic home layout
+mkdir $HOME/{Projects,Documents,Music,Videos} &> /dev/null || true
+
 # Set up formal $DOT_HOME to be used
 # by scripts
-ln -sv $dotfiles_dir $DOT_HOME
+ln -s $dotfiles_dir $DOT_HOME
 
 # Install stow
 echo "* Installing essential packages"
-sudo apt-get install -y ${absolute_essentials[@]}
+sudo apt-get install -y ${absolute_essentials[@]} || exit 1
 
 # Install oh-my-zsh
 echo "* Installing on-my-zsh"
-git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH
+git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH || exit 1
 
 # Set new shell
 echo "* chsh -s zsh"
-stow -S -t $HOME -d $dotfiles_dir zsh && chsh -s $(which zsh)
+stow -S -t $HOME -d $dotfiles_dir zsh && chsh -s $(which zsh) || exit 1
 
 # Finished
 echo "Your dotfiles environment has been properly set up. You will need to re-login for the changes to be applied."
