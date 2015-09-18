1ae3d512c19c913d7aefa9a1980c138514c30886
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-09-18 14:01:24 -0430

    dotstow and dotunstow accept more than one parameter

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -1,4 +1,16 @@
-alias dotstow='stow   --verbose=1 -R -d $DOT_HOME -t $HOME'
-alias dotunstow='stow --verbose=1 -D -d $DOT_HOME -t $HOME'
 alias dotcd='cd $DOT_HOME'
 alias dotu='git -C $DOT_HOME pull' # Update dotfiles
+
+# This will automatically stow directories inside $DOT_HOME
+dotstow() {
+    for i in "$@"; do
+        stow --verbose=1 -R -d $DOT_HOME -t $HOME $i
+    done
+}
+
+# This will automatically unstow directories inside $DOT_HOME
+dotunstow() {
+    for i in "$@"; do
+        stow --verbose=1 -D -d $DOT_HOME -t $HOME $i
+    done
+}
