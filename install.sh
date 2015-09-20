f0ad5721d8e764a0f809f5baa3f16a62a0518f39
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-09-20 14:44:58 -0430

    Fixes on installation script

--- install.sh
+++ install.sh
@@ -53,15 +53,15 @@ done
 [ $answer == 'n' ] && exit 0
 
 # Delete conflicting files (needs better implementation)
-ins_echo "* Deleting conflicting files"
+ins_echo "Deleting conflicting files"
 rm -rf ~/.profile $DOT_HOME $DOT_PATH $ZSH $ZSH_HOME
 
 # DOT_PATH is created before used
-ins_echo "* Creating initial DOT_PATH at $DOT_PATH"
+ins_echo "Creating initial DOT_PATH at $DOT_PATH"
 mkdir -p $DOT_PATH/{lib,bin,libexec,include,share}
 
 # Basic home layout
-ins_echo "* Creating basic home directory layout"
+ins_echo "Creating basic home directory layout"
 mkdir $HOME/{Projects,Documents,Music,Videos} &> /dev/null || true
 
 # Set up formal $DOT_HOME to be used
@@ -69,19 +69,19 @@ mkdir $HOME/{Projects,Documents,Music,Videos} &> /dev/null || true
 ln -s $dotfiles_dir $DOT_HOME
 
 # Install stow
-ins_echo "* Installing essential packages"
+ins_echo "Installing essential packages"
 sudo apt-get update && sudo apt-get install -y "${absolute_essentials[@]}" > /dev/null || exit 1
 
 # Install oh-my-zsh
-ins_echo "* Installing on-my-zsh"
+ins_echo "Installing on-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git $ZSH > /dev/null || exit 1
 
 # Set up essentials
-ins_echo "* Setting up essentials"
+ins_echo "Setting up essentials"
 stow -S -t $HOME -d $dotfiles_dir essentials > /dev/null || exit 1
 
 # Set new shell
-ins_echo "* chsh -s zsh"
+ins_echo "chsh -s zsh"
 stow -S -t $HOME -d $dotfiles_dir zsh && chsh -s $(which zsh) > /dev/null || exit 1
 
 # Finished
