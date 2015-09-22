0e60fc036c04a3ad963d60d74b91fa365964dbbe
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-09-22 16:51:01 -0430

    aliases aren't truly needed any longer

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -1,18 +1,2 @@
 alias dotcd='cd $DOT_HOME'
-alias dotu='git -C $DOT_HOME pull' # Update dotfiles
-alias ds='dotstow'
-alias dus='dotunstow'
 
-# This will automatically stow directories inside $DOT_HOME
-dotstow() {
-    for i in "$@"; do
-        stow --verbose=1 -R -d $DOT_HOME -t $HOME $i
-    done
-}
-
-# This will automatically unstow directories inside $DOT_HOME
-dotunstow() {
-    for i in "$@"; do
-        stow --verbose=1 -D -d $DOT_HOME -t $HOME $i
-    done
-}
