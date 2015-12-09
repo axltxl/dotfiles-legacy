4e636eb86ba31f104b3b961e4eda1278ddf41e4a
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-09 11:06:02 -0430

    commodity aliases for dot-* commands

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -7,3 +7,7 @@ alias dot-reload="echo 'Reloading zsh environment ...'; . ~/.zshenv; . ~/.zshrc"
 
 # dot-env-edit
 alias dot-env-edit="nvim $DOT_ENV_CFG; dot-reload"
+
+# commodity aliases
+alias dee=dot-env-edit
+alias dr=dot-reload
