2fbb934e91877073c12130f277910176de3de625
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-15 20:07:05 -0430

    dee uses EDITOR

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -6,7 +6,7 @@
 alias dot-reload="echo 'Reloading zsh environment ...'; . ~/.zshenv; . ~/.zshrc"
 
 # dot-env-edit
-alias dot-env-edit="nvim $DOT_ENV_CFG; dot-reload"
+alias dot-env-edit="$EDITOR $DOT_ENV_CFG; dot-reload"
 
 # commodity aliases
 alias dee=dot-env-edit
