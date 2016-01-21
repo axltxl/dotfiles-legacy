b00aa1924fb647a5e286c182b4d9383ea4a20d6d
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2016-01-21 11:45:47 -0430

    new alias: dot-env-reload

--- zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
+++ zsh/.zsh/custom/plugins/dotools/dotools.plugin.zsh
@@ -7,7 +7,9 @@ alias dot-reload="echo 'Reloading zsh environment ...'; . ~/.zshenv; . ~/.zshrc"
 
 # dot-env-edit
 alias dot-env-edit="rm -f $DOT_ENV_OUT; $EDITOR $DOT_ENV_CFG; dot-reload"
+alias dot-env-reload="rm -f $DOT_ENV_OUT; dot-reload"
 
 # commodity aliases
 alias dee=dot-env-edit
+alias der=dot-env-reload
 alias dr=dot-reload
