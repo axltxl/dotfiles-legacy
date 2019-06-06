6d9c1b1f3890a4bf818934eb6bb098a2f52a132b
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-06 12:56:34 +0200

    memacs: emacs-dashboard got some love

--- memacs/config.d/splash.el
+++ memacs/config.d/splash.el
@@ -17,13 +17,12 @@
 
 ;; A random dashboard title goes as well
 (setq axltxl/emacs-dashboard-titles
-  [ "Hello world!"
-    "You rock today!"
-    "Because fuck you!, that's why!."
-    "\"Royale with cheese\" - Pulp Fiction, 1994"
-    ])
+  [ "You rock today!"
+    "\"Royale with cheese\" - Pulp Fiction, 1994."
+    "\"Only dead fish go with the flow\" - Andy Hunt. Pragmatic Thinking and Learning."])
 
- ;; The awesome emacs dashboard
+;; The awesome emacs dashboard
+;; https://github.com/emacs-dashboard/emacs-dashboard
 (use-package dashboard
   :config
   ;; Set the title
@@ -36,6 +35,22 @@
 
   ;; Content is not centered by default.
   (setq dashboard-center-content t)
+
+  ;; Set up agenda items from org-mode
+  (add-to-list 'dashboard-items '(agenda) t)
+  (setq show-week-agenda-p t)
+
+  ;; Widgets
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-items '((recents  . 5)
+                          (projects . 5)
+                          (agenda . 5)))
+
+  ;; show info about the packages loaded and the init time
+  (setq dashboard-set-init-info t)
+
+  ;; No footer
+  (setq dashboard-set-footer nil)
 
   ;; Start it up
   (dashboard-setup-startup-hook))
