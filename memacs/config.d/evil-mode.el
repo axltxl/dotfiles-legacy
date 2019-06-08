8e38fbda548538080ce50aa6db0284c4d46688de
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-08 14:49:34 +0200

    memacs: all prefixed key combinations are relabeled on which-key

--- memacs/config.d/evil-mode.el
+++ memacs/config.d/evil-mode.el
@@ -13,13 +13,31 @@
 (defconst axltxl/leader-key "SPC")
 (use-package general
   :demand t
-  :after evil
+  :after (evil which-key)
   :config
     ;; FIXME comment this
     (general-evil-setup t)
 
     ;; FIXME: comment this
     (general-create-definer axltxl/general-leader-def :prefix axltxl/leader-key))
+
+;; All prefixed key combinations are relabeled on which-key
+;; via general
+(general-define-key
+  :prefix axltxl/leader-key
+  :keymaps 'override
+  :states '(normal visual motion emacs)
+  "p"  '(nil :which-key "project")
+  "f"  '(nil :which-key "file")
+  "fe" '(nil :which-key "emacs-conf")
+  "b"  '(nil :which-key "buffer")
+  "w"  '(nil :which-key "window")
+  "g"  '(nil :which-key "git")
+  "j"  '(nil :which-key "jump")
+  "t"  '(nil :which-key "toggle")
+  "o"  '(nil :which-key "org")
+  "oa" '(nil :which-key "org-agenda")
+  "oc" '(nil :which-key "org-capture"))
 
 ;; Prefix definition
 ;; TODO document me if it comes to that
