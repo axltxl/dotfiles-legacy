0c12b29ac2f7c26dc48f3accc73b698051782895
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-09 13:28:10 +0200

    memacs: easily switch to messages and scratch buffers

--- memacs/config.d/buffer-mgmt.el
+++ memacs/config.d/buffer-mgmt.el
@@ -11,15 +11,17 @@
   "fer" 'axltxl/config-restart
 
   ;; Edit main configuration
-  "fed" 'axltxl/config-edit
+  "fed" 'axltxl/init-edit
 
   ;; Edit specific configuration layer
-  "fel" 'axltxl/layer-edit
+  "fel" 'axltxl/config-edit
 
   ;; Buffer management
   "bp" 'previous-buffer
   "bn" 'next-buffer
   "bb" 'switch-to-buffer
+  "bm" (lambda () (interactive)(switch-to-buffer "*Messages*"))
+  "bs" (lambda () (interactive)(switch-to-buffer "*scratch*"))
   "bd" 'evil-delete-buffer
   "bN" 'evil-buffer-new
   "br" 'revert-buffer)
