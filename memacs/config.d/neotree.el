c9c87dc6477225dd6fd32c88e8cc5b9e678a0f09
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-09 11:05:44 +0200

    memacs: NeoTree can be opened (toggled) at projectile project root

--- memacs/config.d/neotree.el
+++ memacs/config.d/neotree.el
@@ -2,7 +2,7 @@
 ;; neotree
 ;;;;;;;;;;;;;;;;;;
 
-(use-package neotree :after general
+(use-package neotree :after (general projectile)
 
   ;; Key bindings for this one
   :general
@@ -27,5 +27,22 @@
   ;; all-the-icons support
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
 
+;; Similar to find-file-in-project,
+;; NeoTree can be opened (toggled) at projectile project root as follows:
+;; https://www.emacswiki.org/emacs/NeoTree
+(defun neotree/project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (projectile-project-root))
+        (file-name (buffer-file-name)))
+    (neotree-toggle)
+    (if project-dir
+      (if (neo-global--window-exists-p)
+        (progn
+          (neotree-dir project-dir)
+          (neotree-find file-name)))
+      (message "Could not find git project root."))))
+
 ;; Prefixed key bindings
 (axltxl/define-key "ft" 'neotree-toggle)
+(axltxl/define-key "pt" 'neotree/project-dir)
