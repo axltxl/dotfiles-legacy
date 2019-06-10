ea96013fa91f1c65bf8b7330c66f9d1035ac4bb4
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-10 10:44:03 +0200

    memacs: config revamp with org-babel :)

--- memacs/init.el
+++ memacs/init.el
@@ -6,117 +6,23 @@
 ;; STATE: experimental
 ;; -----------------------------
 
-;; custom-set-variables is set on another file
-;; https://emacs.stackexchange.com/questions/21394/how-can-i-change-where-the-customize-puts-custom-set-variables
-(setq custom-file (concat user-emacs-directory "custom.el"))
-
-;; Essentials
-;; ----------
-
-;; Main constants
-(defconst axltxl/emacs-init-file  (concat user-emacs-directory "init.el"))
-(defconst axltxl/emacs-config-dir (concat user-emacs-directory "config.d/"))
-
-(defun axltxl/config-get-file (name)
-  "Get path to configuration module"
-  (concat axltxl/emacs-config-dir (format "%s" name) ".el"))
-
-(defun axltxl/config-load (name)
-  "Load configuration module"
-  (load-file (axltxl/config-get-file name)))
-
-(defun axltxl/config-restart ()
-  "Command to reload main configuration"
-  (interactive)
-  (load-file axltxl/emacs-init-file))
-
-(defun axltxl/init-edit ()
-  "Edit init.el configuration file"
-  (interactive)
-  (find-file axltxl/emacs-init-file))
-
-(defun axltxl/config-edit ()
-  "Edit configuration module file"
-  (interactive)
-  (let '(config-file (concat axltxl/emacs-config-dir (read-string "Configuration module name: ") ".el"))
-    (if (file-exists-p config-file)
-      (find-file config-file)
-      (message "Configuration module '%s' does not exist!" config-file))))
-
-;; Packages, packages, packages!
-;; -----------------------------
-;; Source: http://aaronbedra.com/emacs.d/#user-info
-
-;; Setup MELPA stable as the package repo
+;; Setup MELPA as the package repo
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives
   '("melpa" . "https://melpa.org/packages/") t)
 
-;; Define my bootstrap packages :)
-(defvar axltxl/packages '(use-package) "Default packages")
+;; use-package is the only package that is needed at init time.
+;; It's awesome and it will take care of everything from this point onwards.
+(when (not (package-installed-p 'use-package))
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-;; Install default package
-;; ------------------------
-;; When Emacs boots, check to make sure all of the packages
-;; defined in abedra/packages are installed. If not, have ELPA take care of it.
-(require 'cl)
-(defun axltxl/packages-installed-p ()
-  (loop for pkg in axltxl/packages
-     when (not (package-installed-p pkg)) do (return nil)
-     finally (return t)))
-
-(unless (axltxl/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg axltxl/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
-;; This is only needed once, near the top of the file
 (eval-when-compile
-        (require 'use-package)
+  (require 'use-package)
   (setq use-package-always-ensure t)
   (setq use-package-verbose t))
 
-(setq axltxl/config-modules
-  '(
-    ;; The very basics
-    essentials
-    which-key
-
-    ;; The evil section
-    evil
-
-    ;; Basic workflow
-    emacs
-    window
-    buffer
-
-    ;; Text editing must haves
-    text
-    toggles ; UI toggle switches
-
-    ;; Look and feel
-    look-and-feel
-
-    ;; My toolbox
-    org
-    projectile
-    ivy
-    git
-    neotree
-
-    ;; Support for different file types
-    yaml
-
-    ;; Our lovely home screen
-    splash
-    ))
-
-;; Take it from here and load config files
-;; ---------------------------------------
-(dolist (cfg axltxl/config-modules) (axltxl/config-load cfg))
-
-;; Load custom-set-variables file
-(load custom-file 'noerror)
+(require 'org)
+(with-eval-after-load 'org
+  (org-babel-load-file (concat user-emacs-directory "README.org")))
