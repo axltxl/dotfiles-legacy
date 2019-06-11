e7822bbc4caa65a5b469fbae549db57be2cf5657
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2019-06-11 19:24:30 +0200

    memacs is now it is own dedicated repo

--- memacs/config.d/toggles.el
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;
-;; Toggles
-;;;;;;;;;;;;;;
-
-;; Toggle whitespace in current buffer
-(axltxl/define-key "tw" 'whitespace-mode)
-
-;; Toggle display line numbers
-(axltxl/define-key "tl" 'display-line-numbers-mode)
--- memacs/config.d/which-key.el
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; which-key configuration
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package which-key
-  :demand t
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay 0.25))
